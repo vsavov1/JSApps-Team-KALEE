ac43e9300267588e11de0e98034d388653a287f5
ivantodorov91 <ivan.todorov91@abv.bg>
2015-04-24 18:25:26 +0300

    adminView test

--- Blog/scripts/views/adminView.js
+++ Blog/scripts/views/adminView.js
@@ -1,1 +1,21 @@
-console.log(1);
+var app = app || {};
+
+app.adminView = (function() {
+    function AdminView(selector, data) {
+        $(selector).empty();
+        $('#rightSide').empty();
+        $('#center').empty();
+        $.get('templates/home.html', function(template) {
+            var output = Mustache.render(template, data);
+            $(selector).html(output);
+        });
+
+        // Sample solution using mustache - not necessary, can do it with DOM Manipulation as well.
+    }
+
+    return {
+        load: function (selector, data) {
+            return AdminView(selector, data);
+        }
+    }
+}());
